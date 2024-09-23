cc1799de3372b7891df304ebbbf984ae6816b075
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-09-23 23:08:44 +0300

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -4,29 +4,40 @@
 
 from datetime import datetime
 from uuid import uuid4
-import models
+
 
 class BaseModel:
-    
-    def __init__(self): 
+    """Base model class
+    """
+    format = "%Y-%m-%dT%H:%M:%S.%f"
+
+    def __init__(self, *args, **kwargs):
         """initialize BaseModel class"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-    
+        if kwargs or len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(
+                        value, BaseModel.format)
+                elif key != "__class__":
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
     def __str__(self):
         """return string representation of BaseModel."""
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
-    
+
     def save(self):
         """ save instance of Basemodel"""
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
         save_dict = {}
         save_dict = self.__dict__
         save_dict["__class__"] = self.__class__.__name__
         save_dict["created_at"] = self.created_at.isoformat()
         save_dict["updated_at"] = self.updated_at.isoformat()
-        
+
         return save_dict
